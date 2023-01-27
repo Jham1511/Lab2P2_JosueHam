416626041cd89eda1501113a7beb412b15a7be2f
Josue Ham <josue.ham@unitec.edu>
2023-01-27 14:09:06 -0600

    Clase edificio creada

--- src/lab2p2_josueham/Edificio.java
+++ src/lab2p2_josueham/Edificio.java
@@ -5,5 +5,40 @@ import java.util.*;
 
 public class Edificio {
     private int NumPisos, locales;
+    private String direccion;
+
+    public Edificio(int NumPisos, int locales, String direccion) {
+        this.NumPisos = NumPisos;
+        this.locales = locales;
+        this.direccion = direccion;
+    }
+
+    public Edificio() {
+    }
+
+    public int getNumPisos() {
+        return NumPisos;
+    }
+
+    public void setNumPisos(int NumPisos) {
+        this.NumPisos = NumPisos;
+    }
+
+    public int getLocales() {
+        return locales;
+    }
+
+    public void setLocales(int locales) {
+        this.locales = locales;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
     
 }
