df36b3349679c445860da56d7c5a0e2a44c763f3
Josue Ham <josue.ham@unitec.edu>
2023-01-27 14:53:33 -0600

    Metodo del log in creado y menu principal seccion usuarios creado

--- src/lab2p2_josueham/Lab2P2_JosueHam.java
+++ src/lab2p2_josueham/Lab2P2_JosueHam.java
@@ -8,6 +8,8 @@ public class Lab2P2_JosueHam {
 
     public static void main(String[] args) {
         int opcion = 0;
+        ArrayList<Usuario> usuarios = new ArrayList();
+        ArrayList edificios = new ArrayList();
 
         do {
             String menu = "----- Bienvenido al menu del programa -----"
@@ -23,19 +25,67 @@ public class Lab2P2_JosueHam {
 
                 case 1: {
                     String opciones = "Crear Casas/Edificios/Solares\n"
-                            + "\nListar Casas/Edificios/Solares\n"
-                            + "\nModificar Casas/Edificios/Solares\n"
-                            + "\nBorrar Casas/Edificios/Solares\n"
+                            + "\nListar Casas/Edificios/Solares"
+                            + "\nModificar Casas/Edificios/Solares"
+                            + "\nBorrar Casas/Edificios/Solares"
                             + "\nComprar Casas/Edificios/Solares"
                             + "\nIngrese que opcion desea: ";
-                            System.out.println(opciones);
-                            int op = leer.nextInt();
-                            
+                    System.out.println(opciones);
+                    int op = leer.nextInt();
+
                 }//Fin case 1
                 break;
-                
+
+                case 2: {
+
+                }//Fin case 2
+                break;
+
+                case 3: {
+                    int resp = 0;
+                    do {
+                        String men = "Bienvenido al menu de usuarios"
+                                + "\n1 -> Crear Usuario"
+                                + "\n2 -> Iniciar Sesion"
+                                + "\n3 -> Salir";
+                        System.out.println(men);
+                        resp = leer.nextInt();
+
+                      switch (resp){
+                          case 1: {
+                              
+                          }//Fin case 1
+                          break;
+                          
+                          case 2: {
+                              
+                          }//Fin case 2
+                          break;
+                          
+                          default:
+                              System.out.println("Regresando al menu principal...");
+                      }//Fin switch 
+                    } while (resp != 3);
+                }//Fin case 3
+                break;
+
+                default:
+                    System.out.println("Saliendo...");
+
             }//Fin del switch
         } while (opcion != 4);
     }//Fin del main 
 
+    public boolean verifUsuario(ArrayList<Usuario> lista, String username, String contra) {
+        boolean esta = false;
+        for (int i = 0; i < lista.size(); i++) {
+            if (contra.equals(lista.get(i).getPassword()) && username.equals(lista.get(i).getUser())) {
+                esta = true;
+            } else {
+                esta = false;
+            }
+
+        }
+        return esta;
+    }//
 }//Fin de la clase
